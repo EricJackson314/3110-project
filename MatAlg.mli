a5040a5aab9c9876ea78cd4769b5ca83d13236eb
ralphwx <ralph.wang.x@gmail.com>
2020-04-11 10:29:20 -0700

    moved matrix functions to matalg module

---
+++ MatAlg.mli
@@ -0,0 +1,27 @@
+open Matrix
+open Vector
+open Num
+
+module E : Num
+module Mat : Matrix with module E = E
+module Vec : Vector with module E = E
+type vector = Vec.t
+type matrix = Mat.t
+type elem = E.t
+
+val is_square : matrix -> bool
+val ortho : matrix -> matrix
+val ortho_normal : matrix -> matrix
+val row_sp : matrix -> matrix
+val perp : matrix -> matrix
+(* old_basis -> new_basis -> conversion_matrix *)
+val change_basis : matrix -> matrix -> matrix
+val det : matrix -> elem
+val is_singular : matrix -> bool
+val inverse : matrix -> matrix
+val factor_lu : matrix -> matrix * matrix option
+val factor_plu : matrix -> matrix * matrix * matrix
+val eigen : matrix -> (elem * (vector list)) list
+val diag : matrix -> matrix * matrix option
+val svd : matrix -> matrix * matrix * matrix
+
--- Matrix.mli
+++ Matrix.mli
@@ -26,9 +26,6 @@ module type Matrix = sig
   (* [num_cols t] is the number of columns in matrix t *)
   val num_cols : t -> int
   
-  (* [is_square x] is true if x has equal numbers of rows and columns *)
-  val is_square : t -> bool
-  
   (* [entry row col t] is the entry at the given row and column of the matrix.
      Requires row and col to be in bounds, raises OutOfBoundsException
      otherwise *)
@@ -48,71 +45,22 @@ module type Matrix = sig
      all a and b *)
   val transpose : t -> t
   
+  (* [ref mat] is the row echelon form of mat, preserving determinant *)
+  val ref : t -> t
+
+  (* [rref mat] is the reduced row echelon form of mat *)
+  val rref : t-> t
+
   (* [col_sp mat] is [x] if the columns of x form a basis for the columns space
      of mat *)
   val col_sp : t -> t
   
+  (* [pivot_cols mat] is a list of indices of the pivot columns of mat *)
+  val pivot_cols : t -> int list
   (* [nul_sp mat] is [x] if the columns of x form a basis for the null space of
      mat *)
   val nul_sp : t -> t
  
-  (* [ortho mat] is [x] if x and mat have the same column space and x is an
-     orthogonal matrix *)
-  val ortho : t -> t
-
-  (* [ortho_normal mat] is [x] if x and mat have the same column space and x
-     is an orthonormal matrix *)
-  val ortho_normal : t -> t
-
-  (* [perp mat] is [x] if the columns of x form a basis for the largest possible
-     subspace orthogonal to the column space of mat *)
-  val perp : t -> t
-
-  (* [change_basis mat] is [x] if changing from standard basis to the basis
-     given by mat requires left-multiplying by x *)
-  val change_basis : t -> t
-  (* [is_sing mat] is true if mat is a square matrix and is not invertible,
-     false otherwise. *)
-  val is_sing : t -> bool
- 
-  (* [inverse mat] is [x] such that the matrix product mat * x is the identity
-     matrix. Raises SingularMatrixException if mat is singular; raises 
-     DimensionMismatchException if mat is non_square. *)
-  val inverse : t -> t
-  
-  (* [factor_lu mat] is [Some (mat1*mat2)] for lower triangular matrix mat1 and 
-     upper triangular matrix mat2 where (mult mat1 mat2) = mat or [None] if 
-     row exchanges were required to compute lu factorization. *)
-  val factor_lu : t -> t * t option
-  
-  (* [factor_plu mat] is p*l*u for permutation matrix p, lower triangular matrix
-     l, and upper triangular matrix u such that mat is the matrix product 
-     p * l * u *)
-  val factor_plu : t -> t * t * t
-  
-  (* [ref mat] is the reduced echelon form of mat, preserving determinant if mat
-     is a square matrix *)
-  val ref : t -> t
-  
-  (* [rref mat] is the reduced row echelon form of mat *)
-  val rref : t -> t
-  
-  (* [det mat] is the determinant of the matrix mat. Raises
-     DimensionMismatchException if mat is not a square *)
-  val det : t -> elem
-
-  (* [eigen mat] is a list of eigenvalue- eigenvector list pairs, where each
-     eigenvalue is paired with a list of its corresponding eigenvectors.
-     Postcondition: the list contains no duplicate eigenvalues *)
-  val eigen : t -> (elem * vector list) list
-  
-  (* [diag mat] is [Some mat1 * mat2] if mat is diagonalizable and the matrix
-     product mat1 * mat2 * (inverse mat1) = mat, [None] otherwise *)
-  val diag : t -> t * t option
-  
-  (* [is_herm mat] is true if mat is self-adjoint, false otherwise *)
-  val is_herm : t -> bool
-  
   (* [to_column_list mat] is a list containing the columns of mat as vectors, in
      order. *)
   val to_column : t -> vector list
@@ -121,9 +69,6 @@ module type Matrix = sig
      in v. Raises DimensionMismatchException if not all vectors in v are the
      same length *)
   val concat : vector list -> t
-  (* [svd mat] is [u * s * v] if the matrix product u * s * (transpose v) = mat,
-     u and v are orthogonal, and s is a diagonal matrix *)
-  val svd : t -> t * t * t
 end
 
 module type MatrixMaker = functor (Elem : Num) -> Matrix with module E = Elem
